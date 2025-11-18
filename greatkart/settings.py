75f44c6487528fe606daecbcfd79d3a59de35c55
athul raj v <athulraj2002@gmail.com>
2025-11-18 13:04:44 +0530

    django authetication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,3 +134,16 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    
+}
+
+# smtp  configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'athulrajv2002@gmail.com'
+EMAIL_HOST_PASSWORD = 'sikrduoqdwpynjri'
+EMAIL_USE_TLS = True
